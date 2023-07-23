e1ac5579a40c147edaba4491ac700b95b1ef03fd
Ahmed R <90009031+arooble@users.noreply.github.com>
2023-07-23 16:08:32 -0400

    EmployeeService.java

---
+++ EmployeeService.java
@@ -0,0 +1,123 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class EmployeeService {
+
+    HashSet<Employee> empset=new HashSet<Employee>();
+
+    Employee emp1=new Employee(101, "Marcus", 30, "Tester", "CO", 25000);
+    Employee emp2=new Employee(102, "Ahmed", 25, "Developer","IT", 77000);
+    Employee emp3=new Employee(103, "Scottie", 29, "DevOps Eng","Admin", 55000);
+    Employee emp4=new Employee(104, "Max", 27, "System Eng","CO",  40000);
+
+    Scanner sc=new Scanner(System.in);
+    boolean found=false;
+    int id;
+    String name;
+    int age;
+    String department;
+    String role;
+    double sal;
+
+    public EmployeeService() {
+
+        empset.add(emp1);
+        empset.add(emp2);
+        empset.add(emp3);
+        empset.add(emp4);
+
+    }
+
+    //view all employees
+    public void viewAllEmps() {
+        for(Employee emp:empset) {
+            System.out.println(emp);
+        }
+    }
+    //view emp based on there id
+    public void viewEmp(){
+
+        System.out.println("Enter id: ");
+        id=sc.nextInt();
+        for(Employee emp:empset) {
+            if(emp.getId()==id) {
+                System.out.println(emp);
+                found=true;
+            }
+
+        }
+        if(!found) {
+            System.out.println("Employee with this id is not present");
+        }
+    }
+    //update the employee
+    public void updateEmployee() {
+        System.out.println("Enter id: ");
+        id=sc.nextInt();
+        boolean found=false;
+        for(Employee emp:empset) {
+            if(emp.getId()==id) {
+                System.out.println("Enter name: ");
+                name=sc.next();
+                System.out.println("Enter new Salary");
+                sal=sc.nextDouble();
+                emp.setName(name);
+                emp.setSalary(sal);
+                System.out.println("Updated Details of employee are: ");
+                System.out.println(emp);
+                found=true;
+            }
+        }
+        if(!found) {
+            System.out.println("Employee is not present");
+        }
+        else {
+            System.out.println("Employee details updated successfully !!");
+        }
+    }
+    //delete emp
+    public void deleteEmp() {
+        System.out.println("Enter id");
+        id=sc.nextInt();
+        boolean found=false;
+        Employee empdelete=null;
+        for(Employee emp:empset) {
+            if(emp.getId()==id) {
+                empdelete=emp;
+                found=true;
+            }
+        }
+        if(!found) {
+            System.out.println("Employee is not present");
+        }
+        else {
+            empset.remove(empdelete);
+            System.out.println("Employee deleted successfully!!");
+        }
+    }
+    //add emp
+    public void addEmp() {
+        System.out.println("Enter ID:");
+        id=sc.nextInt();
+        System.out.println("Enter Name");
+        name=sc.next();
+        System.out.println("Enter Age");
+        age=sc.nextInt();
+        System.out.println("Enter Role");
+        role=sc.next();
+        System.out.println("Enter Department");
+        department=sc.next();
+        System.out.println("Enter Salary");
+        sc.nextDouble();
+
+        Employee emp=new Employee(id, name, age, role, department, sal);
+
+        empset.add(emp);
+        System.out.println(emp);
+        System.out.println("Employtee addeed successsfully");
+
+    }
+
+}
